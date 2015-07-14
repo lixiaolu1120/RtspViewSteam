6ca1fef97de3ec8a58612a25e1398b8b5c474465
lixiaolu <erevus@yeah.net>
2015-07-14 15:30:25 +0800

    add localSeekBarchangeListener

---
+++ app/src/main/java/com/xmu/rtsp/LocalSeekbarChageListener.java
@@ -0,0 +1,20 @@
+package com.xmu.rtsp;
+
+import android.widget.SeekBar;
+
+public class LocalSeekbarChageListener implements SeekBar.OnSeekBarChangeListener {
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
--- app/src/main/java/com/xmu/rtsp/RtspActivity.java
+++ app/src/main/java/com/xmu/rtsp/RtspActivity.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
@@ -21,9 +20,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import static android.view.KeyEvent.KEYCODE_BACK;
-import static android.view.MotionEvent.ACTION_CANCEL;
-import static android.view.MotionEvent.ACTION_DOWN;
-import static android.view.MotionEvent.ACTION_UP;
 import static android.widget.Toast.LENGTH_SHORT;
 import static com.xmu.rtsp.Constants.TAG;
 
@@ -32,12 +28,6 @@ public class RtspActivity extends Activity {
     private VideoView videoView;
     private MediaController controller;
     private SeekBar seekBar;
-    private Button cmdUp;
-    private Button cmdDown;
-    private Button cmdForward;
-    private Button cmdBack;
-    private Button cmdLeft;
-    private Button cmdRight;
     private Timer timer;
     private TimerTask task;
 
@@ -89,13 +79,6 @@ public class RtspActivity extends Activity {
         seekBar = (SeekBar) findViewById(R.id.progressBar);
         seekBar.setMax(100);
 
-        cmdUp = (Button) findViewById(R.id.cmdUp);
-        cmdDown = (Button) findViewById(R.id.cmd_down);
-        cmdForward = (Button) findViewById(R.id.cmd_forward);
-        cmdBack = (Button) findViewById(R.id.cmd_back);
-        cmdLeft = (Button) findViewById(R.id.cmd_left);
-        cmdRight = (Button) findViewById(R.id.cmd_right);
-
         videoView = (VideoView) this.findViewById(R.id.rtsp_player);
         videoView.setFocusable(false);
         controller = new MediaController(this);
@@ -103,151 +86,6 @@ public class RtspActivity extends Activity {
     }
 
     private void initListener() {
-        cmdUp.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                int action = motionEvent.getAction();
-                switch (action) {
-                    case ACTION_DOWN:
-                        Configuration.keepPositionUpCmd(RtspActivity.this, true);
-                        Log.i(TAG, "Up is pressed!");
-                        break;
-
-                    case ACTION_UP:
-                        Configuration.keepPositionUpCmd(RtspActivity.this, false);
-                        Log.i(TAG, "Up is not pressed!");
-                        break;
-
-                    case ACTION_CANCEL:
-                        Configuration.keepPositionUpCmd(RtspActivity.this, false);
-                        Log.i(TAG, "Up is not pressed!");
-                        break;
-                }
-                return false;
-            }
-        });
-
-        cmdDown.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                int action = motionEvent.getAction();
-                switch (action) {
-                    case ACTION_DOWN:
-                        Configuration.keepPositionDownCmd(RtspActivity.this, true);
-                        Log.i(TAG, "down is pressed!");
-                        break;
-
-                    case ACTION_UP:
-                        Configuration.keepPositionDownCmd(RtspActivity.this, false);
-                        Log.i(TAG, "down is not pressed!");
-                        break;
-
-                    case ACTION_CANCEL:
-                        Configuration.keepPositionDownCmd(RtspActivity.this, false);
-                        Log.i(TAG, "down is not pressed!");
-                        break;
-                }
-                return false;
-            }
-        });
-
-        cmdForward.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-
-                int action = motionEvent.getAction();
-                switch (action) {
-                    case ACTION_DOWN:
-                        Configuration.keepPositionForwardCmd(RtspActivity.this, true);
-                        Log.i(TAG, "Forward is pressed!");
-                        break;
-
-                    case ACTION_UP:
-                        Configuration.keepPositionForwardCmd(RtspActivity.this, false);
-                        Log.i(TAG, "Forward is not pressed!");
-                        break;
-
-                    case ACTION_CANCEL:
-                        Configuration.keepPositionForwardCmd(RtspActivity.this, false);
-                        Log.i(TAG, "Forward is not pressed!");
-                        break;
-                }
-                return false;
-            }
-        });
-
-        cmdBack.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                int action = motionEvent.getAction();
-                switch (action) {
-                    case ACTION_DOWN:
-                        Configuration.keepPositionBackCmd(RtspActivity.this, true);
-                        Log.i(TAG, "BACK is pressed!");
-                        break;
-
-                    case ACTION_UP:
-                        Configuration.keepPositionBackCmd(RtspActivity.this, false);
-                        Log.i(TAG, "BACK is not pressed!");
-                        break;
-
-                    case ACTION_CANCEL:
-                        Configuration.keepPositionBackCmd(RtspActivity.this, false);
-                        Log.i(TAG, "BACK is not pressed!");
-                        break;
-                }
-                return false;
-            }
-        });
-
-        cmdLeft.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-
-                int action = motionEvent.getAction();
-                switch (action) {
-                    case ACTION_DOWN:
-                        Configuration.keepPositionLeftCmd(RtspActivity.this, true);
-                        Log.i(TAG, "LEFT is pressed!");
-                        break;
-
-                    case ACTION_UP:
-                        Configuration.keepPositionLeftCmd(RtspActivity.this, false);
-                        Log.i(TAG, "LEFT is not pressed!");
-                        break;
-
-                    case ACTION_CANCEL:
-                        Configuration.keepPositionUpCmd(RtspActivity.this, false);
-                        Log.i(TAG, "LEFT is not pressed!");
-                        break;
-                }
-                return false;
-            }
-        });
-
-        cmdRight.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                int action = motionEvent.getAction();
-                switch (action) {
-                    case ACTION_DOWN:
-                        Configuration.keepPositionRightCmd(RtspActivity.this, true);
-                        Log.i(TAG, "RIGHT is pressed!");
-                        break;
-
-                    case ACTION_UP:
-                        Configuration.keepPositionRightCmd(RtspActivity.this, false);
-                        Log.i(TAG, "RIGHT is not pressed!");
-                        break;
-
-                    case ACTION_CANCEL:
-                        Configuration.keepPositionRightCmd(RtspActivity.this, false);
-                        Log.i(TAG, "RIGHT is not pressed!");
-                        break;
-                }
-                return false;
-            }
-        });
 
         playButton.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
@@ -256,7 +94,7 @@ public class RtspActivity extends Activity {
             }
         });
 
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBar.setOnSeekBarChangeListener(new LocalSeekbarChageListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int position, boolean b) {
                 Log.i(TAG, "SeekBar :" + position);
@@ -264,14 +102,8 @@ public class RtspActivity extends Activity {
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 Toast.makeText(getApplicationContext(), "当前油门值为：" + seekBar.getProgress(), LENGTH_SHORT).show();
-//                Configuration.keepThrottleStatus(RtspActivity.this, seekBar.getProgress());
             }
         });
     }
