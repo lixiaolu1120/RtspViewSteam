5ff94b06765e3b4628a4f8070ccc457eb1b7fbce
lixiaolu <erevus@yeah.net>
2015-07-17 10:33:47 +0800

    morning commit

--- app/build/generated/source/r/debug/com/xmu/rtsp/R.java
+++ app/build/generated/source/r/debug/com/xmu/rtsp/R.java
@@ -14,17 +14,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int fly_stop=0x7f050002;
-        public static final int forward_back_bar=0x7f050008;
-        public static final int forward_back_text=0x7f050007;
-        public static final int left_right_bar=0x7f050004;
-        public static final int left_right_text=0x7f050003;
+        public static final int fly_start=0x7f050004;
+        public static final int fly_stop=0x7f050005;
+        public static final int forward_back_bar=0x7f05000b;
+        public static final int forward_back_text=0x7f05000a;
+        public static final int left_right_bar=0x7f050007;
+        public static final int left_right_text=0x7f050006;
+        public static final int reset=0x7f050001;
         public static final int rtsp_player=0x7f050000;
-        public static final int start_play=0x7f050001;
-        public static final int throttle_bar=0x7f050006;
-        public static final int throttle_text=0x7f050005;
-        public static final int toward_Left_right_bar=0x7f05000a;
-        public static final int toward_Left_right_text=0x7f050009;
+        public static final int start_play=0x7f050002;
+        public static final int stop_play=0x7f050003;
+        public static final int throttle_bar=0x7f050009;
+        public static final int throttle_text=0x7f050008;
+        public static final int toward_Left_right_bar=0x7f05000d;
+        public static final int toward_Left_right_text=0x7f05000c;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- app/src/main/java/com/xmu/rtsp/RtspActivity.java
+++ app/src/main/java/com/xmu/rtsp/RtspActivity.java
@@ -31,7 +31,7 @@ import static com.xmu.rtsp.Configuration.keepTowardLeftRightStatus;
 import static com.xmu.rtsp.Constants.TAG;
 
 public class RtspActivity extends Activity {
-    private Button playButton;
+    private Button startPlay;
     private VideoView videoView;
     private MediaController controller;
     private SeekBar throttleBar;
@@ -45,38 +45,46 @@ public class RtspActivity extends Activity {
     private TextView forwardBack;
     private TextView towardLeftRight;
     private Button flyStop;
+    private Button stopPlay;
+    private Button flyStart;
+    private Button reset;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        timer = new Timer();
-        initTask();
-        timer.schedule(task, 50, 100);
 
         initView();
+
+        reset();
+
         initListener();
     }
 
     private void initView() {
-        playButton = (Button) this.findViewById(R.id.start_play);
+        startPlay = (Button) this.findViewById(R.id.start_play);
+        stopPlay = (Button) findViewById(R.id.stop_play);
+
+        flyStart = (Button) findViewById(R.id.fly_start);
         flyStop = (Button) findViewById(R.id.fly_stop);
+
+        reset = (Button) findViewById(R.id.reset);
 
         throttle = (TextView) findViewById(R.id.throttle_text);
         throttleBar = (SeekBar) findViewById(R.id.throttle_bar);
-        throttleBar.setProgress(50);
+        throttleBar.setProgress(Configuration.getThrottleStatus(RtspActivity.this));
 
         leftRight = (TextView) findViewById(R.id.left_right_text);
         leftRightBar = (SeekBar) findViewById(R.id.left_right_bar);
-        leftRightBar.setProgress(50);
+        leftRightBar.setProgress(Configuration.getLeftRightStatus(RtspActivity.this));
 
         forwardBack = (TextView) findViewById(R.id.forward_back_text);
         forwardBackBar = (SeekBar) findViewById(R.id.forward_back_bar);
-        forwardBackBar.setProgress(50);
+        forwardBackBar.setProgress(Configuration.getForwardBackStatus(RtspActivity.this));
 
         towardLeftRight = (TextView) findViewById(R.id.toward_Left_right_text);
         towardLeftRightBar = (SeekBar) findViewById(R.id.toward_Left_right_bar);
-        towardLeftRightBar.setProgress(50);
+        towardLeftRightBar.setProgress(Configuration.getTowardLeftRightStatus(RtspActivity.this));
 
         videoView = (VideoView) this.findViewById(R.id.rtsp_player);
         videoView.setFocusable(false);
@@ -85,10 +93,23 @@ public class RtspActivity extends Activity {
     }
 
     private void initListener() {
-        playButton.setOnClickListener(new Button.OnClickListener() {
+        startPlay.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 Log.i(TAG, "press Play BT");
                 PlayRtspStream("rtsp://192.168.43.1:8086");
+            }
+        });
+
+        flyStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startFly();
+
+                if (timer != null) return;
+                timer = new Timer();
+                initTask();
+
+                timer.schedule(task, 50, 100);
             }
         });
 
@@ -99,32 +120,48 @@ public class RtspActivity extends Activity {
             }
         });
 
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reset();
+            }
+        });
+
         throttleBar.setOnSeekBarChangeListener(new LocalSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int position, boolean b) {
                 super.onProgressChanged(seekBar, position, b);
+
+                position = position + 1;
                 throttle.setText("油门：" + (position - 50));
                 keepThrottleStatus(RtspActivity.this, position);
             }
+
         });
 
         leftRightBar.setOnSeekBarChangeListener(new LocalSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int position, boolean b) {
                 super.onProgressChanged(seekBar, position, b);
+                position = position + 1;
                 if (position == 50) {
                     leftRight.setText("水平：0");
                     return;
                 }
+
                 leftRight.setText("水平：" + ((position - 50) > 0 ? "右" : "左") + ((position - 50) > 0 ? position - 50 : 50 - position));
                 Configuration.keepLeftRightStatus(RtspActivity.this, position);
             }
+
         });
+
 
         forwardBackBar.setOnSeekBarChangeListener(new LocalSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int position, boolean b) {
                 super.onProgressChanged(seekBar, position, b);
+                position = position + 1;
+
                 if (position == 50) {
                     forwardBack.setText("前后：0");
                     return;
@@ -133,46 +170,82 @@ public class RtspActivity extends Activity {
                 forwardBack.setText("前后：" + ((position - 50) > 0 ? "前" : "后") + ((position - 50) > 0 ? position - 50 : 50 - position));
                 Configuration.keepForwardBackStatus(RtspActivity.this, position);
             }
+
         });
 
         towardLeftRightBar.setOnSeekBarChangeListener(new LocalSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int position, boolean b) {
                 super.onProgressChanged(seekBar, position, b);
+                position = position + 1;
+
                 if (position == 50) {
                     towardLeftRight.setText("自转：0");
                     return;
                 }
-                towardLeftRight.setText("自转：" + ((position - 50) > 0 ? "右" : "左") + ((position - 50) > 0 ? position - 50 : 50 - position));
+
+                towardLeftRight.setText("自转：" + ((position - 50) >= 0 ? "右" : "左") + ((position - 50) > 0 ? position - 50 : 50 - position));
                 Configuration.keepTowardLeftRightStatus(RtspActivity.this, position);
             }
+
         });
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        stopFly();
     }
 
     @Override
     protected void onDestroy() {
         videoView.pause();
+
+        if (timer == null) return;
+        timer.purge();
         timer.cancel();
+        timer = null;
         super.onDestroy();
     }
 
+    private void startFly() {
+        leftRightBar.setProgress(99);
+        keepLeftRightStatus(RtspActivity.this, 110);
+
+        forwardBackBar.setProgress(0);
+        keepForwardBackStatus(RtspActivity.this, 1);
+
+        throttleBar.setProgress(0);
+        keepThrottleStatus(RtspActivity.this, 1);
+
+        towardLeftRightBar.setProgress(99);
+        keepTowardLeftRightStatus(RtspActivity.this, 110);
+    }
+
     private void stopFly() {
-        throttleBar.setProgress(50);
-        keepThrottleStatus(RtspActivity.this, 50);
-
-        forwardBackBar.setProgress(50);
-        keepForwardBackStatus(RtspActivity.this, 50);
-
-        leftRightBar.setProgress(50);
+        leftRightBar.setProgress(49);
         keepLeftRightStatus(RtspActivity.this, 50);
 
-        towardLeftRightBar.setProgress(50);
+        forwardBackBar.setProgress(49);
+        keepForwardBackStatus(RtspActivity.this, 50);
+
+        throttleBar.setProgress(0);
+        keepThrottleStatus(RtspActivity.this, 1);
+
+        towardLeftRightBar.setProgress(49);
+        keepTowardLeftRightStatus(RtspActivity.this, 50);
+    }
+
+    private void reset() {
+        leftRightBar.setProgress(49);
+        keepLeftRightStatus(RtspActivity.this, 50);
+
+        forwardBackBar.setProgress(49);
+        keepForwardBackStatus(RtspActivity.this, 50);
+
+        throttleBar.setProgress(49);
+        keepThrottleStatus(RtspActivity.this, 50);
+
+        towardLeftRightBar.setProgress(49);
         keepTowardLeftRightStatus(RtspActivity.this, 50);
     }
 
@@ -198,7 +271,7 @@ public class RtspActivity extends Activity {
             public void run() {
                 try {
                     Socket socket = new Socket("192.168.43.1", 5038);
-                    String message = getLeftRightStatus(RtspActivity.this) + "," + getThrottleStatus(RtspActivity.this) + "," + getForwardBackStatus(RtspActivity.this) + "," + getTowardLeftRightStatus(RtspActivity.this);
+                    String message = getLeftRightStatus(RtspActivity.this) + "," + getForwardBackStatus(RtspActivity.this) + "," + getThrottleStatus(RtspActivity.this) + "," + getTowardLeftRightStatus(RtspActivity.this);
                     Log.i("lixiaolu", message);
                     BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                     PrintWriter out = new PrintWriter(wr, true);
